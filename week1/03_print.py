e45372b8d6306fa54cf233c46a6dc44715e56d88
fengloop186 <socbel25@gmail.com>
2026-04-21 10:59:57 +0700

    Day 3 - print

---
+++ week1/03_print.py
@@ -0,0 +1,11 @@
+#Sử dụng print để in ra thông tin
+print("Hello World!")
+a = 100
+b = 200
+c = 300
+print (a,b,c, sep = " - ")  # In ra giá trị của a, b, c với dấu phân cách là " - "
+print("Giá trị của a là:", a)  # In ra giá trị của a
+print("Giá trị của b là:", b)  # In ra giá trị của b
+print("Giá trị của c là:", c)  # In ra giá trị của c
+print(a,b,end = " -\n ")
+print(c)
