966efd47c472b650103207c30fee84ebeab969fd
fengloop186 <socbel25@gmail.com>
2026-05-02 12:00:34 +0700

    getting started

---
+++ 01_Getting_Started/01/variable.py
@@ -0,0 +1,17 @@
+#Calculate the sum of two numbers
+s = 18062005
+t = 13032007
+c = s + t
+print(c)
+
+age = 18
+name = "FengLoop"
+PI = 3.14
+print(age)
+print(name)
+print(PI)
+
+#DataType
+print(type(age))
+print(type(name))
+print(type(PI))
--- week1/04_datatype2.py
+++
@@ -1,9 +0,0 @@
-# Chuyển về phân số 
-#Fraction là kiểu dữ liệu phân số trong Python, dùng để lưu số dưới dạng tử số / mẫu số một cách chính xác
-from fractions import*
-#Fraction(<tử số>, <mẫu số>)
-frac1 = Fraction(6,9)
-frac2 = Fraction(5,10)
-frac3 = frac1 + frac2
-
-print(frac3)
